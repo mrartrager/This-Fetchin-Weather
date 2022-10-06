1f0e63768c8949bc19abaab052b977c4b5d8074c
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-10-06 18:32:13 -0500

    added API KEY to JavaScript_Started HTML Structure

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
 let theDate = moment().format('LL');
 document.getElementById("momentDate").innerHTML = theDate;
 
+let apiKey ="ec494eef8fd8461dc250656042aa352f";
+let weatherData = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + "ec494eef8fd8461dc250656042aa352f";
