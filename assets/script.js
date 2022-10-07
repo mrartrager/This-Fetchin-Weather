e9cd95922b25dac0157f10410b778c09fe74a913
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-10-07 17:06:11 -0500

    added variables to fetch in JavaScript_fixed ID's in HTML

--- assets/script.js
+++ assets/script.js
@@ -4,16 +4,22 @@ document.getElementById("momentDate").innerHTML = theDate;
 let searchBtn=document.getElementById('search-btn');
 
 let apiKey ="ec494eef8fd8461dc250656042aa352f";
-let weatherData = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + "ec494eef8fd8461dc250656042aa352f";
+let weatherData = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearched+ "&appid=" + apiKey";
 
-fetch(weatherData, {
-  method: 'GET', //GET is the default.
-  credentials: 'same-origin', // include, *same-origin, omit
-  redirect: 'follow', // manual, *follow, error
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+fetch(weatherData)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(weatherNow){
+
+        let presentTemp=weatherNow.main.temp;
+        let presentWindSpeed=weatherNow.wind.speed;
+        let presenthumidity=weatherNow.main.humidity;
+
+
+        let currentCity= document.getElementById('currentCity')
+        let currentTemp=document.getElementById('currentTemperature')
+        let currentWind=document.getlElementById('currentWindSpeed')
+        let currentHumidity= document.getElementById('currentHumidity')
+         
+    })
